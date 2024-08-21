f3841efa8a4d2ea195363c02a5cd25bdbd8db1e6
Simonlee3z <95853975+Simonlee3z@users.noreply.github.com>
2024-08-21 09:29:02 +0800

    Delete build directory
    delete build file

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ljh/postgraduate/opengl_renderer
-# Build directory: /Users/ljh/postgraduate/opengl_renderer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
