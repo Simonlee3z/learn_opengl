77b5587c1e9b11e300a31104b26b8d6c4010d979
Simonlee3z <95853975+Simonlee3z@users.noreply.github.com>
2024-08-21 10:38:15 +0800

    Delete build directory
    delete again

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/learn_opengl/learn_opengl
-# Build directory: D:/learn_opengl/learn_opengl/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
